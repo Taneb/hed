5a8946c7937064ff75ae43538d96d2a217f6c12f
= <nvd1234@gmail.com>
2017-09-23 09:47:36 +0100

    Fix up warnings

--- src/Hed/Command/Print.hs
+++ src/Hed/Command/Print.hs
@@ -2,7 +2,6 @@ module Hed.Command.Print where
 
 import qualified Data.Vector as V
 import qualified Data.Text.IO as T
-import Text.Megaparsec
 import Text.Megaparsec.Char
 
 import Hed.Command
@@ -16,5 +15,12 @@ instance IsCommand Print where
         V.mapM_ T.putStrLn (V.unsafeSlice (from-1) (to-from) body)
         return (Right (body, Just to))
       else return (Left "Invalid address")
+  runCommand [line] body Print =
+    if 1 <= line && line <= V.length body
+      then do
+        T.putStrLn (V.unsafeIndex body (line-1))
+        return (Right (body, Just line))
+      else return (Left "Invalid address")
+  runCommand [] _ Print = undefined
 
   parseCommand _ = Print <$ char 'p'
--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,8 @@
 {-# LANGUAGE GADTs #-}
 module Main where
 
-import Control.Applicative
-import Data.Char
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import Data.Proxy
-import qualified Data.Vector as V
-import Data.Void
-import Text.Megaparsec
-import Text.Megaparsec.Char
-
-import Hed
 import Hed.Address
-import Hed.Address.Literal
 import Hed.Command
-import Hed.Command.Print
 
 data Instruction = Instruction {
   address :: [SomeAddress],
